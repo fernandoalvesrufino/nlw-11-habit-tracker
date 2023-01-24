55f6d981ece56bca55f9a3bc2de9fb50d0a01ae1
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2023-01-24 14:13:46 -0300

    Adicionando pontuacao maxima

--- script.js
+++ script.js
@@ -1,27 +1,65 @@
-const form = document.querySelector('#form-habits')
-const nlwSetup = new NLWSetup(form)
-const button = document.querySelector("header button")
+const mario = document.getElementById("mario");
+const pipe = document.getElementById("pipe");
+const gameover = document.getElementById("gameover");
+const pontuacao = document.getElementById("pontuacao");
+const record = document.getElementById("record");
+const atualizar = document.getElementById("atualizar");
 
-button.addEventListener("click", add)
-form.addEventListener("change", save)
+let pontuacaoAtual = 0
+let pontuacaoMaxima = localStorage.getItem("maxScore") || 0;
 
-function add() {
-  // const today = "19/01"
-  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
-  const dayExists = nlwSetup.dayExists(today)
-  
-  if(dayExists) {
-    alert("Dia já incluso!")
-    return
+const numberToString = parseInt(pontuacaoMaxima)
+record.textContent = "Record: " + numberToString.toFixed(0)
+
+function updateScore(pontos) {
+  if (pontos > pontuacaoMaxima) {
+    pontuacaoMaxima = pontos;
+    localStorage.setItem("maxScore", pontuacaoMaxima);
+  }
+}
+
+function jump () {
+  if (mario.classList != "jump"){
+    mario.classList.add("jump");
+    
+    setTimeout(function () {
+      mario.classList.remove("jump");
+    }, 500);
+  }
+} 
+
+let loop = setInterval(function () {
+
+  const pipePosition = parseInt(window.getComputedStyle(pipe).getPropertyValue("left"));
+  const marioPosition = parseInt(window.getComputedStyle(mario).getPropertyValue("bottom"));
+  pontuacaoAtual = pontuacaoAtual + 0.09;
+  const info = "Pontuação: " + pontuacaoAtual.toFixed(1); 
+  pontuacao.textContent = info
+    
+
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
+    
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
+    mario.src = './img/mario-gameover.webp';
+    mario.style.width = '120px';
+    mario.style.marginLeft = "40px";
+    
+    gameover.style.marginLeft = "35%"
+    atualizar.style.marginLeft = "48%"
+
+    updateScore(pontuacaoAtual);
+
+    clearInterval(loop);
   }
 
-  nlwSetup.addDay(today)
-}
+}, 10);
 
-function save() {
-  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
-}
+document.addEventListener("keydown", function (event) {
+  jump()
+});
 
-const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
-nlwSetup.setData(data)
-nlwSetup.load()
+
